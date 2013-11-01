7aabae0dc5b133c68453a67500c8c946c23c1b9a
G <igauravsehrawat@gmail.com>
2013-11-01 11:08:51 +0530

    perimeter arguments rmonk

---
+++ modules.rb
@@ -0,0 +1,33 @@
+module Perimeter
+  def perimeter
+    sides.inject(0) { |sum, side| sum + side }
+  end
+end
+
+class Rectangle
+  # Your code here
+  include Perimeter
+  
+  def initialize(length, breadth)
+    @length = length
+    @breadth = breadth
+  end
+
+  def sides
+    [@length, @breadth, @length, @breadth]#forgot this was array
+  end
+end
+
+class Square
+  # Your code here
+  include Perimeter
+  
+  def initialize(side)
+    @side = side
+  end
+
+  def sides
+    [@side, @side, @side, @side]#forgot this was an array implementation
+  end
+end
+
